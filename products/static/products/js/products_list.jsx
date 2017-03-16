a384487b5da35f927c3f0ce0a5118f846d039d56
syren520 <xuyurong@yahoo.com>
2017-03-16 00:14:56 -0700

    Add initialization of this.state in constructor to solve race competition

--- products/static/products/js/products_list.jsx
+++ products/static/products/js/products_list.jsx
@@ -6,12 +6,16 @@ class Greeter extends Component{
 //{config.greetText}
    constructor() {
       super();
+      // necessary to initialize state otherwise will be race competition between render and fetch
+      this.state = {
+         data: []
+      }
    };
-
 
   //TIP: One of the benifit to use  arrow function '() => {}' instead of 'function() {}', arrow function is not binding with this
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions#No_binding_of_arguments
   componentDidMount() {
+        console.log('>>>>>>>>');
     fetch('/products/list')
       .then((response) => {
         return response.json()
@@ -20,11 +24,13 @@ class Greeter extends Component{
       console.log(json);
         this.setState({data: json});
       });
+    console.log('>>>>>>>>');
   };
 
 
+
+
   render() {
-    console.log(this.state.data);
       return (
         <div className="row">
           <div className="large-12 columns">
@@ -42,11 +48,10 @@ class Greeter extends Component{
                 console.log(product.description)
                 {/*For some reason, when you return multi line dom, if you don't use bracket to include all your return, it won't work*/}
                 return (
-                  <div className="large-4 medium-4 columns end">
-                    <div>
+                  //Each child in an array or iterator should have a unique "key" prop.
+                  <div className="large-4 medium-4 columns end" key={i}>
                       <a href="#">{product.name}</a>
                       <p className="product-content">{product.description}</p>
-                      </div>
                   </div>)
               })}
             </div>
