f342fac528f689f928144d889a02f1c696a56304
syren520 <xuyurong@yahoo.com>
2017-03-17 08:12:58 -0700

    Extract raw query to const update database schema

--- products/views.py
+++ products/views.py
@@ -1,8 +1,27 @@
 from django.http import JsonResponse
 from database import databaseQueries
 
+GET_ALL_PRODUCTS = """
+WITH
+    group_tags AS (
+    SELECT
+        pt.product_id,
+        array_agg(t.name)
+    FROM
+        products_tags AS pt
+        JOIN tags AS t ON pt.tag_id = t.id
+    GROUP BY pt.product_id
+    )
+SELECT
+    p.*,
+    gt.array_agg AS tags
+FROM
+    products AS p
+    LEFT JOIN group_tags AS gt ON gt.product_id = p.id;
+    """
+
 def get_all_products():
-    return databaseQueries.exe_raw_query("SELECT name, description FROM products")
+    return databaseQueries.exe_raw_query(GET_ALL_PRODUCTS)
 
 
 def getProductList(request):
--- rawQuery/schema.sql
+++ rawQuery/schema.sql
@@ -1,7 +1,85 @@
-CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL ,
-    description TEXT
+CREATE TABLE tags (
+    id   SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
 );
 
-INSERT INTO products (name, description) VALUES ('AAA', '1111'), ('BBB', '2222');
+CREATE TABLE products (
+    id           SERIAL PRIMARY KEY,
+    name         TEXT    NOT NULL,
+    description  TEXT,
+    unit         TEXT    NOT NULL,
+    unit_price   REAL    NOT NULL,
+    store_amount INTEGER NOT NULL
+);
+
+CREATE TABLE products_tags (
+    id         SERIAL PRIMARY KEY,
+    product_id INTEGER REFERENCES products (id),
+    tag_id     INTEGER REFERENCES tags (id)
+);
+CREATE UNIQUE INDEX product_tag
+    ON products_tags (product_id, tag_id);
+
+INSERT INTO tags (name) VALUES ('dog'), ('cat'), ('food'), ('medicine'), ('accessory');
+INSERT INTO products (name, description, unit, unit_price, store_amount) VALUES
+    ('doggy-food-1', 'this is kind of food for peppy', 'bag', 5, 20),
+    ('doggy-food-2', 'this is kind of food elder dog', 'bag', 15, 20),
+    ('doggy-food-3', 'this is kind of food 3', 'bag', 15, 6),
+    ('doggy-food-4', 'this is kind of food 4', 'bag', 10, 9),
+    ('doggy-food-5', 'this is kind of food 5', 'bag', 7, 1),
+    ('doggy-toy-1', 'this is kind of toy', 'piece', 25, 15),
+    ('doggy-medicine-1', 'this is kind of medicine1', 'bottle', 45, 15),
+    ('doggy-medicine-2', 'this is kind of medicine2', 'bottle', 27, 10),
+    ('catty-food-1', 'this is kind of cat food 1', 'bag', 1, 15),
+    ('catty-food-2', 'this is kind of cat food 2', 'can', 2, 13),
+    ('catty-food-3', 'this is kind of cat food 3', 'can', 1.8, 12),
+    ('catty-toy-1', 'this is kind of cat toy 1', 'can', 15, 2),
+    ('catty-medicine-1', 'this is kind of medicine1', 'bottle', 90, 9),
+    ('catty-medicine-2', 'this is kind of medicine2', 'bottle', 55, 15);
+
+INSERT INTO products_tags (product_id, tag_id) VALUES
+    (1,1),
+    (2,1),
+    (3,1),
+    (4,1),
+    (5,1),
+    (6,1),
+    (7,1),
+    (8,1),
+    (9,2),
+    (10,2),
+    (11,2),
+    (12,2),
+    (13,2),
+    (14,2),
+    (1,3),
+    (2,3),
+    (3,3),
+    (4,3),
+    (5,3),
+    (9,3),
+    (10,3),
+    (11,3),
+    (7,4),
+    (8,4),
+    (13,4),
+    (14,4),
+    (6,5),
+    (12,5);
+
+WITH
+    group_tags AS (
+    SELECT
+        pt.product_id,
+        array_agg(t.name)
+    FROM
+        products_tags AS pt
+        JOIN tags AS t ON pt.tag_id = t.id
+    GROUP BY pt.product_id
+    )
+SELECT
+    p.*,
+    gt.array_agg AS tags
+FROM
+    products AS p
+    LEFT JOIN group_tags AS gt ON gt.product_id = p.id;
