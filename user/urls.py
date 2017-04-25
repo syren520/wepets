fc74e2e626b3187a713a514a8a1ee1bd13ff491d
syren520 <xuyurong@yahoo.com>
2017-04-25 01:47:11 -0700

    add api register new user

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'login', views.postLogin)
+    url(r'login', views.postLogin),
+    url(r'register', views.postCreateNewUser)
 ]
--- user/views.py
+++ user/views.py
@@ -4,8 +4,9 @@ from django.http import JsonResponse, HttpResponse
 
 
 from django.contrib.auth import login, logout
+from user.models import User
 from auth.backend import emailAuthBackend
-
+from django.db.utils import IntegrityError as usernameDuplicatedError
 
 def postLogin(request):
     backend = emailAuthBackend()
@@ -14,5 +15,20 @@ def postLogin(request):
         login(request, user)
     else:
         return HttpResponse(status=401)
+
+    return  HttpResponse(status=200)
+
+
+def postCreateNewUser(request):
+    try:
+        user = User.objects.create_user('aabcetwrt@gmail.com', 'ttttt')
+    except usernameDuplicatedError:
+        return HttpResponse(content='email already existed', status=400)
+    # backend = emailAuthBackend()
+    # user = backend.authenticate(request=request, email='aab@gmail.com', password='ttttt')
+    if user is not None:
+        login(request, user)
+    else:
+        return HttpResponse(status=500)
 
     return  HttpResponse(status=200)
