27354613650a6eb28f3e105a5acb679f000f5a7d
syren520 <xuyurong@yahoo.com>
2017-04-26 21:23:29 -0700

    Update user login and logout api

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
-    url(r'login', views.postLogin),
-    url(r'register', views.postCreateNewUser)
+    url(r'login', views.post_login),
+    url(r'logout', views.get_logout),
+    url(r'register', views.post_create_new_user)
 ]
--- user/views.py
+++ user/views.py
@@ -1,34 +1,44 @@
 from django.shortcuts import render
-
 from django.http import JsonResponse, HttpResponse
-
-
 from django.contrib.auth import login, logout
 from user.models import User
 from auth.backend import emailAuthBackend
 from django.db.utils import IntegrityError as usernameDuplicatedError
 
-def postLogin(request):
+
+def post_login(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    if not username or not password:
+        HttpResponse(status=400)
+
     backend = emailAuthBackend()
-    user = backend.authenticate(request=request, email='aab@gmail.com', password='ttttt')
+    user = backend.authenticate(request=request, email=username, password=password)
     if user is not None:
         login(request, user)
     else:
         return HttpResponse(status=401)
+    return HttpResponse(status=200)
 
-    return  HttpResponse(status=200)
 
+def post_create_new_user(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    if not username or not password:
+        return HttpResponse(status=400)
 
-def postCreateNewUser(request):
     try:
-        user = User.objects.create_user('aabcetwrt@gmail.com', 'ttttt')
+        user = User.objects.create_user(username, password)
     except usernameDuplicatedError:
         return HttpResponse(content='email already existed', status=400)
-    # backend = emailAuthBackend()
-    # user = backend.authenticate(request=request, email='aab@gmail.com', password='ttttt')
     if user is not None:
         login(request, user)
     else:
         return HttpResponse(status=500)
 
-    return  HttpResponse(status=200)
+    return HttpResponse(status=200)
+
+
+def get_logout(request):
+    logout(request)
+    return HttpResponse(status=200)
